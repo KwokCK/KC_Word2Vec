d368388beb3d124f4201920f37a7c3de967073fe
KC <davis_kwoks@yahoo.com.hk>
2018-08-28 18:00:22 +0800

    KwokCK

--- removal.py
+++ removal.py
@@ -2,7 +2,7 @@ import io
 from nltk.corpus import stopwords
 from nltk.tokenize import word_tokenize
 #word_tokenize accepts a string as an input, not a file.
-stop_words = set(stopwords.words('english'))
+stop_words = set(stopwords.words('english'))- {'for', 'do', 'while'}
 file1 = open("pdfdata/outputPDF.text")
 line = file1.read()# Use this to read file content as a stream:
 words = line.split()
